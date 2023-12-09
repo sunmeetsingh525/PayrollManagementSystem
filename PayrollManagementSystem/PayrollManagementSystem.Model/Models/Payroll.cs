c9bbfe8f9a19395b082761431b32122226a86816
Sunmeet Singh <sunmeetsingh257@gmail.com>
2023-12-08 21:19:23 -0500

    Added payroll controller class to calculate total payout of an employee.

---
+++ PayrollManagementSystem/PayrollManagementSystem.Model/Models/Payroll.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PayrollManagementSystem.Model.Models
+{
+    public class Payroll
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please, select employee.")]
+        public int EmployeeId { get; set; }
+
+        [Required(ErrorMessage = "Please, provied working day.")]
+        public int NumberOfDaysWorked { get; set; }
+
+        [Required(ErrorMessage = "Please, provied deductions amount.")]
+        public double Deductions { get; set; }
+
+        [Required(ErrorMessage = "Please, provied total payment.")]
+        public double TotalPayment { get; set; }
+
+        public Employee Employee { get; set; }
+    }
+}
