3aa2ebf44dcf45eb4d2ea2d14aa306ef38189b9d
Sunmeet Singh <sunmeetsingh257@gmail.com>
2023-12-08 23:43:20 -0500

    Added ListViewModel.

---
+++ PayrollManagementSystem/PayrollManagementSystem.Model/ViewModels/SelectListViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace PayrollManagementSystem.Model.ViewModels
+{
+    public class SelectListViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
