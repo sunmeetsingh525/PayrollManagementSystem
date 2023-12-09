0539a388b4e9c3f9b00c46b3dca3a86db9d7e85b
Sunmeet Singh <sunmeetsingh257@gmail.com>
2023-12-08 23:42:07 -0500

    Updated payroll viewmodel.

---
+++ PayrollManagementSystem/PayrollManagementSystem.Model/ViewModels/PayrollCreateModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PayrollManagementSystem.Model.ViewModels
+{
+    public class PayrollCreateModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please, select employee.")]
+        public int EmployeeId { get; set; }
+
+        [Required(ErrorMessage = "Please, provied working day.")]
+        public int NumberOfDaysWorked { get; set; }
+
+        [Required(ErrorMessage = "Please, provied deductions amount.")]
+        public double Deductions { get; set; }
+
+        [Required(ErrorMessage = "Please, provied total payment.")]
+        public double TotalPayment { get; set; }
+    }
+}
