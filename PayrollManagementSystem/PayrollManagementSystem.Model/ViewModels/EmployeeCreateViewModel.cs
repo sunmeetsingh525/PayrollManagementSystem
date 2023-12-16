8aca70a19f1f039108b1890f7b954b6cd5ccf14c
Sunmeet Singh <sunmeetsingh257@gmail.com>
2023-12-15 23:02:12 -0500

    Minor UI improvememnts.

--- PayrollManagementSystem/PayrollManagementSystem.Model/ViewModels/EmployeeCreateViewModel.cs
+++ PayrollManagementSystem/PayrollManagementSystem.Model/ViewModels/EmployeeCreateViewModel.cs
@@ -6,19 +6,19 @@ namespace PayrollManagementSystem.Model.ViewModels
     {
         public int Id { get; set; }
 
-        [Required(ErrorMessage = "Please, provide code.")]
+        [Required(ErrorMessage = "Please, provied code.")]
         [StringLength(30, MinimumLength = 3)]
         public string Code { get; set; }
 
-        [Required(ErrorMessage = "Please, provide first name.")]
+        [Required(ErrorMessage = "Please, provied first name.")]
         [StringLength(30, MinimumLength = 2)]
         public string FirstName { get; set; }
 
-        [Required(ErrorMessage = "Please, provide last name.")]
+        [Required(ErrorMessage = "Please, provied last name.")]
         [StringLength(30, MinimumLength = 2)]
         public string LastName { get; set; }
 
-        [Required(ErrorMessage = "Please, provide phone number.")]
+        [Required(ErrorMessage = "Please, provied phone number.")]
         [StringLength(30, MinimumLength = 2)]
         public string PhoneNumber { get; set; }
         public string RelationshipStatus { get; set; }
@@ -27,7 +27,7 @@ namespace PayrollManagementSystem.Model.ViewModels
         public int GenderId { get; set; }
         public int Age { get; set; }
 
-        [Required(ErrorMessage = "Please, provide daily rate.")]
+        [Required(ErrorMessage = "Please, provied daily rate.")]
         public double DailyRate { get; set; }
 
         [Required(ErrorMessage = "Please, select payment method.")]
